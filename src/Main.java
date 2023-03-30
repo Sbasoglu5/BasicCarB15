a1492820b3f979ef73b7e05db14c806ef45f1728
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-03-29 21:24:21 -0400

    the color feature has been added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         BasicStructure audi=new BasicStructure();
@@ -9,5 +11,11 @@ public class Main {
         x.cylinder="8";
         x.size="1234cc";
         x.weight="6543k";
+
+        Color col=new Color();
+        col.colorLights="True";
+        col.colorTire="false";
+        col.colorWindows="True";
+
     }
 }
